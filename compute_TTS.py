bd87eaaf09fa1c98aa563a5edf49aa029e92a5bc
jamaps <jeffallen89@gmail.com>
2021-05-14 16:57:14 -0400

    refactor and update input of coords

--- compute_TTS.py
+++ compute_TTS.py
@@ -5,24 +5,50 @@ import requests
 import time
 import pandas as pd
 
-# subsetting TTS trips by mode (Walk, Drive, Bicycle)
+# subsetting TTS trips by
 def read_subset_tts(mode, path):
-    df = pd.read_csv(path)
-    df = df[df["mode"] == mode]
+
     return df
 
-# joining in XY coords (based on geog - "ct" or "da") to prep for routing
-def join_coords(geog, mode, od_path):
+# joining in XY coords (based on geog - "ct" or "da") and mode (Walk, Drive, Bicycle)
+def trip_stats_tts(geog, mode):
 
-    df = read_subset_tts(mode, od_path)
-    xyhome = pd.read_csv("coordinates/" + geog + "_2016_pts_pop.csv")
-    xyother = pd.read_csv("coordinates/" + geog + "_2016_pts_geom.csv")
+    # load in data
+    df = pd.read_csv("survey_data/od_for_export.csv", dtype = str)
+    df = df[df["mode"] == mode]
 
+    # load in coords, and reduce columns
+    xyhome = pd.read_csv("coordinates/" + geog + "_2016_pts_pop.csv", dtype = str)
+    xyother = pd.read_csv("coordinates/" + geog + "_2016_pts_geom.csv", dtype = str)
     xyhome = xyhome[["X","Y","id"]]
     xyother = xyother[["X","Y","id"]]
 
-    print(xyhome)
-    print(xyother)
-    print(df)
+    # home to other
+    dfho = df[(df["orig_type"] == "Home") & (df["dest_type"] == "Other")]
+    dfho = dfho.merge(xyhome, how="left", left_on='orig_loc', right_on="id")
+    dfho = dfho.rename(columns={"X": "Xi", "Y": "Yi"})
+    del dfho["id"]
+    print(dfho)
 
-join_coords("ct","Walk","survey_data/od_for_export.csv")
+    # home to Home
+    dfhh = df[(df["orig_type"] == "Home") & (df["dest_type"] == "Home")]
+
+    # other to Home
+    dfoh = df[(df["orig_type"] == "Other") & (df["dest_type"] == "Home")]
+
+    # other to other
+    dfoo = df[(df["orig_type"] == "Other") & (df["dest_type"] == "Other")]
+
+
+
+    # other to home
+
+
+    # other to other
+
+
+    # home to home
+
+
+
+trip_stats_tts("da","Walk")
